41e06b16a47d042b8f9fbac6b00a8e9e3eea7a5a
jeovazero <jeovapereira2000@live.com>
2020-10-12 00:06:10 -0300

    style: indent the code

--- android-licenses/android-licenses.nix
+++ android-licenses/android-licenses.nix
@@ -1,20 +1,20 @@
 # licenses for toolsVersion = "26.1.1";
 let
-	nixpkgs = import ../nix/pinned.nix;
-	inherit (nixpkgs) pkgs lib;
-	inherit (builtins) hashString readFile mapAttrs;
+  nixpkgs = import ../nix/pinned.nix;
+  inherit (nixpkgs) pkgs lib;
+  inherit (builtins) hashString readFile mapAttrs;
 
 
-	allLicenses = {
+  allLicenses = {
     "android-googletv-license" = ./android-googletv-license;
-		"android-sdk-license" = ./android-sdk-license;
-		"android-sdk-arm-dbt-license" = ./android-sdk-arm-dbt-license;
-		"android-sdk-preview-license" = ./android-sdk-preview-license;
-		"mips-android-sysimage-license" = ./mips-android-sysimage-license;
-		"google-gdk-license" = ./google-gdk-license;
-	};
+    "android-sdk-license" = ./android-sdk-license;
+    "android-sdk-arm-dbt-license" = ./android-sdk-arm-dbt-license;
+    "android-sdk-preview-license" = ./android-sdk-preview-license;
+    "mips-android-sysimage-license" = ./mips-android-sysimage-license;
+    "google-gdk-license" = ./google-gdk-license;
+  };
 
-	licenseToHash = name: license: (hashString "sha1" (readFile license));
-	licensesMap = mapAttrs licenseToHash allLicenses;
+  licenseToHash = name: license: (hashString "sha1" (readFile license));
+  licensesMap = mapAttrs licenseToHash allLicenses;
 in
-	licensesMap
+  licensesMap
--- shell.nix
+++ shell.nix
@@ -1,49 +1,49 @@
 { pinnedNixpkgs ? import ./nix/pinned.nix }:
 let
-	nixpkgs = pinnedNixpkgs {
-		config = {
-			android_sdk.accept_license = true;
-		};
-	};
+  nixpkgs = pinnedNixpkgs {
+    config = {
+      android_sdk.accept_license = true;
+    };
+  };
 
-	inherit (nixpkgs) pkgs lib;
-	inherit (pkgs) jdk flutter;
+  inherit (nixpkgs) pkgs lib;
+  inherit (pkgs) jdk flutter;
 
   android = pkgs.androidenv.composeAndroidPackages {
-		toolsVersion = "26.1.1";
-		platformVersions = [ "28" ];
-		abiVersions = [ "x86" "x86_64"];
-	};
+    toolsVersion = "26.1.1";
+    platformVersions = [ "28" ];
+    abiVersions = [ "x86" "x86_64"];
+  };
 
   licensesMap = import ./android-licenses/android-licenses.nix;
 
-	licenceSymbolic = licenseName: licenseHash:
-		let
-			licenseFile = pkgs.writeText licenseName ("\n"+licenseHash);
-		in
-			''
-				ln -s ${licenseFile} $out/libexec/android-sdk/licenses/${licenseName}
-			'';
+  licenceSymbolic = licenseName: licenseHash:
+  let
+    licenseFile = pkgs.writeText licenseName ("\n"+licenseHash);
+  in
+  ''
+    ln -s ${licenseFile} $out/libexec/android-sdk/licenses/${licenseName}
+  '';
 
   licensesSymbolicList = lib.attrsets.mapAttrsToList licenceSymbolic licensesMap;
 
-	installPhaseAdditional = ''
-		mkdir -p $out/libexec/android-sdk/licenses
-	'' + (builtins.concatStringsSep "\n" licensesSymbolicList);
+  installPhaseAdditional = ''
+    mkdir -p $out/libexec/android-sdk/licenses
+  '' + (builtins.concatStringsSep "\n" licensesSymbolicList);
 
-	androidsdk = android.androidsdk.overrideAttrs (oldAttrs: {
-		installPhase = oldAttrs.installPhase + installPhaseAdditional;
-	});
+  androidsdk = android.androidsdk.overrideAttrs (oldAttrs: {
+    installPhase = oldAttrs.installPhase + installPhaseAdditional;
+  });
 in
- pkgs.mkShell {
-	buildInputs = [
-    flutter
-	  androidsdk
-    jdk
-  ];
+  pkgs.mkShell {
+    buildInputs = [
+      flutter
+      androidsdk
+      jdk
+    ];
 
-	shellHook = ''
-		export ANDROID_HOME=${androidsdk}/libexec/android-sdk
-		export JAVA_HOME=${jdk.home}
-	'';
-}
+    shellHook = ''
+      export ANDROID_HOME=${androidsdk}/libexec/android-sdk
+      export JAVA_HOME=${jdk.home}
+    '';
+  }
