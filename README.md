bdb452153472787cd3442ec4f2dbfc60afed4b30
jeovazero <jeovapereira2000@live.com>
2020-10-12 00:01:44 -0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Meu env de desenvolvimento android
+
+### Flutter + Nix
+
+(testado somente no Debian 10)
+
+Precisa de Nix instalado.
+
+#### 1. Intalando o env
+
+```sh
+$  nix-shell
+```
+
+vai baixar tudo (flutter, android-sdk), então deve demorar.
+
+#### 2. Fazendo `flutter doctor`
+
+Deve dar o erro:
+
+```
+** Message: 23:48:32.926: Requires Linux version >= 3.19 built with CONFIG_USER_NS
+** Message: 23:48:32.940: Run: sudo sysctl -w kernel.unprivileged_userns_clone=1
+```
+
+Resolva com:
+
+```
+$ sudo sysctl -w kernel.unprivileged_userns_clone=1
+```
+
+#### 3. Reconhecendo um dispositivo android conectado
+
+1. Ative o modo desenvolvedor
+2. Ative o modo debug
+3. Conecte com o modo "Transferir Arquivo" ou algo parecido
+4. Execute `sudo usermod -aG plugdev $LOGNAME`
+5. Faça logout, ou abra outro terminal.
+
+Pronto!
+
+```sh
+$  nix-shell
+$  flutter doctor
+```
+
+vai usar tudo da cache, então deve ser rapido.
+
+![](./demo.jpg)
---
+++ shell.nix
@@ -0,0 +1,49 @@
+{ pinnedNixpkgs ? import ./nix/pinned.nix }:
+let
+	nixpkgs = pinnedNixpkgs {
+		config = {
+			android_sdk.accept_license = true;
+		};
+	};
+
+	inherit (nixpkgs) pkgs lib;
+	inherit (pkgs) jdk flutter;
+
+  android = pkgs.androidenv.composeAndroidPackages {
+		toolsVersion = "26.1.1";
+		platformVersions = [ "28" ];
+		abiVersions = [ "x86" "x86_64"];
+	};
+
+  licensesMap = import ./android-licenses/android-licenses.nix;
+
+	licenceSymbolic = licenseName: licenseHash:
+		let
+			licenseFile = pkgs.writeText licenseName ("\n"+licenseHash);
+		in
+			''
+				ln -s ${licenseFile} $out/libexec/android-sdk/licenses/${licenseName}
+			'';
+
+  licensesSymbolicList = lib.attrsets.mapAttrsToList licenceSymbolic licensesMap;
+
+	installPhaseAdditional = ''
+		mkdir -p $out/libexec/android-sdk/licenses
+	'' + (builtins.concatStringsSep "\n" licensesSymbolicList);
+
+	androidsdk = android.androidsdk.overrideAttrs (oldAttrs: {
+		installPhase = oldAttrs.installPhase + installPhaseAdditional;
+	});
+in
+ pkgs.mkShell {
+	buildInputs = [
+    flutter
+	  androidsdk
+    jdk
+  ];
+
+	shellHook = ''
+		export ANDROID_HOME=${androidsdk}/libexec/android-sdk
+		export JAVA_HOME=${jdk.home}
+	'';
+}
